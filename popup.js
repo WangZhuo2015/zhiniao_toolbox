370cf6ab1da0a6fc0d1f2d9a8008ce5ce12f5c36
王卓 <zhuowangwork@gmail.com>
2023-06-19 15:46:42 +0800

    文件导入
    文件导入

--- popup.js
+++ popup.js
@@ -1,12 +1,3 @@
-// let changeSpeed = document.getElementById('changeSpeed');
-document.getElementById('changeSpeed').onclick = function (element) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      { code: 'document.querySelector("#course-video_html5_api").playbackRate=16.0' });
-  });
-};
-
 document.addEventListener('DOMContentLoaded', function() {
   const dataDiv = document.getElementById('dataDiv');
   chrome.storage.local.get(['question_dic'], function(result) {
@@ -16,4 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
           dataDiv.innerText = JSON.stringify(result.question_dic, null, 2);
       }
   });
+
+  document.getElementById('changeSpeed').onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: 'document.querySelector("#course-video_html5_api").playbackRate=16.0' });
+    });
+  };
+
+  document.getElementById('inputFile').addEventListener('change', function() {
+    var file = this.files[0];
+    var reader = new FileReader();
+  
+    reader.onload = function(event) {
+        var jsonObject = JSON.parse(event.target.result);
+        chrome.storage.local.set({question_dic: jsonObject}, function() {
+            console.log('Question data is updated with new JSON file.');
+        });
+    };
+  
+    reader.onerror = function() {
+        console.log('An error occurred while reading the JSON file.');
+    };
+  
+    reader.readAsText(file);
+  });
+  
 });
